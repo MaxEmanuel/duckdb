904f02dd85e911faf04143557a960b50a3be0255
Maximilian Schuele <m.schuele@tum.de>
2023-08-25 16:42:36 +0200

    autodiff

--- src/planner/binder/expression/bind_function_expression.cpp
+++ src/planner/binder/expression/bind_function_expression.cpp
@@ -155,15 +155,11 @@ BindResult ExpressionBinder::BindLambdaFunction(FunctionExpression &function, Sc
 
 	// get the logical type of the children of the list
 	auto &list_child = BoundExpression::GetExpression(*function.children[0]);
-	if (list_child->return_type.id() != LogicalTypeId::LIST && list_child->return_type.id() != LogicalTypeId::SQLNULL &&
-	    list_child->return_type.id() != LogicalTypeId::UNKNOWN) {
-		return BindResult(" Invalid LIST argument to " + function.function_name + "!");
-	}
-
-	LogicalType list_child_type = list_child->return_type.id();
-	if (list_child->return_type.id() != LogicalTypeId::SQLNULL &&
-	    list_child->return_type.id() != LogicalTypeId::UNKNOWN) {
-		list_child_type = ListType::GetChildType(list_child->return_type);
+	LogicalType list_child_type = list_child.expr->return_type.id();
+	if (list_child.expr->return_type.id() != LogicalTypeId::SQLNULL &&
+	    list_child.expr->return_type.id() != LogicalTypeId::UNKNOWN &&
+		list_child.expr->return_type.id() == LogicalTypeId::LIST) {
+		list_child_type = ListType::GetChildType(list_child.expr->return_type);
 	}
 
 	// bind the lambda parameter
